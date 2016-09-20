56d5c2fecac8a41952aef6217bb703f41190c720
inna <minyailo.inna@gmail.com>
2016-09-21 00:56:07 +0300

    init

---
+++ src/com/brainacad/learningManagementSystem/Student.java
@@ -0,0 +1,11 @@
+package com.brainacad.learningManagementSystem;
+
+/**
+ * Created by go-go-hamster on 9/20/2016.
+ */
+public class Student {
+    int id;
+    String firstName, lastName;
+    String[] courses, tasks;
+
+}
---
+++ src/com/brainacad/learningManagementSystem/Teacher.java
@@ -0,0 +1,11 @@
+package com.brainacad.learningManagementSystem;
+
+/**
+ * Created by go-go-hamster on 9/20/2016.
+ */
+public class Teacher {
+    private int id;
+    private String firstName, lastName;
+    private String[] courses;
+
+}
